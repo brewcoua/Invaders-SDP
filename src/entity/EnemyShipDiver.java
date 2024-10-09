58172c55d180bd536f2d111ab5bcc3ed5d98bcc4
John Kim <dudghks070505@gmail.com>
2024-10-09 23:52:35 +0900

    Adjusted diver attack interval

--- src/entity/EnemyShipDiver.java
+++ src/entity/EnemyShipDiver.java
@@ -9,8 +9,8 @@ import java.awt.*;
 public class EnemyShipDiver extends EnemyShip {
 
     private static final int POINTS = 50;
-    private final int DIVE_INTERVAL = 7500;
-    private final int DIVE_VARIANCE = 2500;
+    private final int DIVE_INTERVAL = 4000;
+    private final int DIVE_VARIANCE = -1000;
     public static final int SPEED_X = 10;
     public static final int SPEED_DIVE = 3;
     private Cooldown diveCooldown;
